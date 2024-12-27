c7b096ee0414b0ae9556837cacefffc23d159528
otanodesignco <otanodesign@gmail.com>
2024-12-27 15:49:56 -0600

    removed uneed files

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,35 +1,18 @@
 uniform float uTime;
-uniform sampler2D uNoiseTexture;
 
 in vec2 vUv;
-in vec3 worldNormals;
-in vec3 viewDirection;
-in vec3 worldPosition;
-in vec3 normals;
-
-#include ./lib/uv/sphereMask.glsl
-#include ./lib/uv/gradientMask.glsl
 
 void main()
 {
 
     vec2 uv = vUv;
-
     float time = uTime;
 
-    float mask = sphereMask( uv, vec2( 0.5 ), 0.5, 0.8 );
+   
 
-    vec3 gradientMask = vec3( gradientMask( uv, vec2( 0.9, 0.00001 ), 0, true ) );
-
-
-    vec3 color = vec3( 0.8, uv );
-
-    vec4 colorFinal = vec4( gradientMask, 1.0 );
-
-
-    gl_FragColor = colorFinal;
+    gl_FragColor = vec4( uv, 0.8, 1.0 );
 
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
-
+    
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,22 +1,11 @@
 
 out vec2 vUv;
-out vec3 worldNormals;
-out vec3 viewDirection;
-out vec3 worldPosition;
-out vec3 normals;
 
 void main()
 {
-    vec4 normalsWorld = modelMatrix * vec4( normal, 0.0 );
-    vec4 positionWorld = modelMatrix * vec4( position, 1.0 );
-    vec3 directionView = cameraPosition - position;
 
     gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 
     vUv = uv;
-    worldNormals = normalsWorld.xyz;
-    viewDirection = directionView;
-    worldPosition = positionWorld.xyz;
-    normals = normal;
-
+    
 }
